08383bd8bd40fa835d01230e95758aa85978d9ce
Ryu JiHoon <dbwglns123@gmail.com>
2020-05-04 06:11:09 +0900

    Add dataset.csv parsing and some minor touch with cmd input & output

--- MP_Team_regression/MP_Team_regression/main.cpp
+++ MP_Team_regression/MP_Team_regression/main.cpp
@@ -1,60 +1,126 @@
 #include <stdio.h>
-#include<vector>
-#include<stdlib.h>
+#include <vector>
+#include <stdlib.h>
 #include <numeric>
 #include "DS_definitions.h"
 #include "PolynomialRegression.h"
 #include "MultipleRegression.h"
 #include "MultipleRegressionParallelized.h"
+#include "CSVReader.h"
+
+#define numRows 9880
+#define numStats 11 // number of game stats from dataset
 
 using namespace std;
 
-#define pf(x, c) c[0] + x*c[1] + x*x*c[2] +  x*x*x*c[3]
-#define mf(x,c) c[0] + x[0]*c[1] + x[1]*c[2] + x[2]*c[3]
+// Simply calculating multi variable function
+double calMF(vector<double> &x, vector<double> &coeff) {
+	double ret = coeff[0];
+	for (int i = 0; i < x.size(); ++i) {
+		ret += x[i] * coeff[i + 1];
+	}
+	return ret;
+}
+
+// for input control
+void pressAny() {
+	printf("\nPress any key to continue...\n");
+	int ch;
+	while ((ch = getchar()) != '\n' && ch != EOF);
+	getchar();
+}
 
 int main() {
-
-	PolynomialRegression<double> pr;
 	MultipleRegression<double> mr;
 	MultipleRegressionP<double> mrp;
 
-	vector<double> x{ 1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63, 1.65, 1.68, 1.70, 1.73, 1.75, 1.78, 1.80, 1.83 };
-	vector<double> y{ 52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93, 61.29, 63.11, 64.47, 66.28, 68.10, 69.92, 72.19, 74.46 };
-	vector<vector<double>> xx;
-	int order = 3;
-	vector<double> coeffs(order,0);
-	vector<double> coeffsP(order,0);
+	vector<double> coeffs(40, 0);		// coefficient
+	vector<double> coeffsP(40, 0);		// coefficient for Polynomial Regression
+	int selectX[numStats]{}, selectY{0};	// statArr to represent user selection
+
+	// vector <int> x, y;		// x's for game stats. y's for blueWins
+
+	// CSV Parsing from dataset.csv
+	// CSVReader from open source project, dataset from kaggle data forum
+	io::CSVReader<numStats> in("dataset.csv");		// 9880 rows, 40 columns in total. 4930 rows when blueWins == 1
+	in.read_header(io::ignore_extra_column, "blueWins", "blueWardsPlaced", "blueWardsDestroyed", "blueTowersDestroyed",
+		"blueKills", "blueDeaths", "blueAssists", "blueTotalExperience", "blueTotalMinionsKilled", "blueExperienceDiff", "blueGoldDiff");
+
+	// User input
+	string statStr[numStats]{ "blueWins", "blueWardsPlaced", "blueWardsDestroyed", "blueTowersDestroyed",
+		"blueKills", "blueDeaths", "blueAssists", "blueTotalExperience", "blueTotalMinionsKilled", "blueExperienceDiff", "blueGoldDiff" };
+	printf("[Available game stats for calculation]\n");
+	for (int i = 0; i < numStats; ++i)
+		printf("[%2d] %-23s\n", i + 1, statStr[i].c_str());
+	printf("\nPlease Select stats by number to include on independent variables(=X) (input '0' to continue) : ");
 	
+	int inpt, inptCnt = 0;
+	while (~scanf("%d", &inpt)) {
+		if (inpt == 0) break;
+		else if (inpt > 0 && inpt <= numStats) { selectX[inptCnt++] = inpt; }
+		else printf("please select a proper number\n");
+	}
+	
+	printf("Now, select ONE stats to be a range set(=Y) : ");
+	while (!selectY && ~scanf("%d", &inpt)) {
+		if (inpt > 0 && inpt <= numStats) { selectY = inpt; }
+		else printf("please select a proper number\n");
+	} pressAny();
 
-	for (int i = 0; i < 15;i++) {
-		double xi = x[i];
-		vector<double> elem{xi, xi*xi, xi*xi*xi};	//다중 회귀 테스트를 위한 다항회귀 입력
-		xx.push_back(elem);
+
+	vector<vector<double>>	dataX{};	// dataX: game stats
+	vector<double>			dataY{};	// dataY: blueWins itself
+	// blueWins, blueWardsPlaced, blueWardsDestroyed, blueTowersDestroyed, blueKills, blueDeaths, blueAssists, blueTotalExperience, blueTotalMinionsKilled, blueExperienceDiff, blueGoldDiff;
+	double stats[numStats]{};
+	while (in.read_row(stats[0], stats[1], stats[2], stats[3], stats[4], stats[5], stats[6], stats[7], stats[8], stats[9], stats[10])) {
+		// Setting independent variables set and result set
+		vector<double> tmp;
+		for (int i = 0; i < inptCnt; ++i)
+			tmp.push_back(stats[selectX[i] - 1]);
+		
+		dataX.push_back(tmp);
+		dataY.push_back(stats[selectY - 1]);
 	}
 
-	//pr.fitIt(x, y, order, coeffs);	//다항 회귀
-	mr.fitIt(xx, y, coeffs);				//다중 회귀
-
-	for (double i : coeffs)		//상수항부터 계수
-		printf("%f ",i);
+	// Multiple Regression
+	printf("\n[ Multiple Regression on Progress... ]\n");
+	mr.fitIt(dataX, dataY, coeffs);
+	printf("Completed function: f(x) = %f", coeffs[0]);
+	for (int i = 0; i < inptCnt; ++i)
+		printf(" + %f * x%d", coeffs[i + 1], i);
 	printf("\n");
 
-	mrp.fitIt(xx, y, coeffsP);				//다중 회귀
+	// Multiple Regression Parallelized
+	printf("\n[ Parallelized Multiple Regression on Progress... ]\n");
+	mrp.fitIt(dataX, dataY, coeffsP);
+	printf("Completed function: f(x) = %f", coeffsP[0]);
+	for (int i = 0; i < inptCnt; ++i)
+		printf(" + %f * x%d", coeffsP[i + 1], i);
+	printf("\n\n");
 
-	for (double i : coeffsP)		//상수항부터 계수
-		printf("%f ", i);
-	printf("\n");
+	// Verifying results with real dataset
+	printf("\n\n[ Verifying with Dataset ]\n"); 
+	printf("How many dataset do you want to verify? : ");
+	while (~scanf("%d", &inpt)) {
+		if (inpt > 0 && inpt < numRows) break;
+		else if (inpt >= numRows) printf("I'm afraid there's only 9880 rows in dataset...\n");
+		else printf("please select a proper number\n");
+	} pressAny();
 
-	//입력값과 수식값 비교
-	for (int i = 0; i < 11; ++i) {
-		printf("x(");
-		for (double d : xx[i])
-			printf("%.2f ", d);
-		printf(") ");
+	printf("\nGiven Factors were \nX { ");
+	for(int i = 0; i < inptCnt; ++i)
+		printf("%s, ", statStr[selectX[i] - 1].c_str());
+	printf("}, \nY { %s }\n\n", statStr[selectY - 1].c_str());
 
-		printf("y(%.2f) f(x)=%f (p)f(x)=%f\n", y[i], mf(xx[i], coeffs), mf(xx[i], coeffsP));
+	for (int i = 0; i < inpt; ++i) {
+		printf("X[%3d]{", i);
+		for (double d : dataX[i])
+			printf("%6.2f ", d);
+		printf("}");
 
+		printf(" | Y[%3d](%8.2f) | f(x)=%12.3f (p)f(x)=%12.3f\n", i, dataY[i], calMF(dataX[i], coeffs), calMF(dataX[i], coeffsP));
 	}
-	getchar();
+
+	pressAny();
 	return 0;
 }
